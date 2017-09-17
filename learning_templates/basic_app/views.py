7701a98bcda24b31512bb3e5158f3077efb9353b
srikarveeramallu <32032925+srikarveeramallu@users.noreply.github.com>
2017-09-17 15:59:49 +0530

    First
    First Commit

---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context_dict = {'text':'hello world','number':100}
+    return render(request,'basic_app/index.html',context_dict)
+
+def other(request):
+    return render(request,'basic_app/other.html')
+
+def relative(request):
+    return render(request,'basic_app/relative_url_template.html')
